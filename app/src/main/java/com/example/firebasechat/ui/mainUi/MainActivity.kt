be4d0483c3c0aee0329080af3a2e25e594a4f4b3
Aashif <aashif.dev@gmail.com>
2023-05-21 01:03:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/firebasechat/ui/mainUi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firebasechat.ui.mainUi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebasechat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
